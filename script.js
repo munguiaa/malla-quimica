4b3de60613bf8404ebc39e30b25b86c58d237f9d
munguiaa <munguia.mendoza.estefania@gmail.com>
2025-07-14 15:33:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
 const materias = {
-  "Álgebra Superior": { semestre: 1, desbloquea: ["Termodinámica"] },
-  "Cálculo I": { semestre: 1, desbloquea: ["Cálculo II", "Ecuaciones Diferenciales"] },
-  "Ciencia y Sociedad": { semestre: 1 },
-  "Física I": { semestre: 1, desbloquea: ["Física II", "Lab. de Física"] },
-  "Química General I": { semestre: 1, desbloquea: ["Química General II"] },
-  "La Universidad Libre de Violencia de Género": { semestre: 1 },
-
-  "Cálculo II": { semestre: 2 },
-  "Estructura de la Materia": { semestre: 2, desbloquea: ["Química Inorgánica I", "Química Orgánica I"] },
-  "Física II": { semestre: 2, desbloquea: ["Fundamentos de Espectroscopia"] },
-  "Lab. de Física": { semestre: 2, desbloquea: ["Metrología"] },
-  "Química General II": { semestre: 2, desbloquea: ["Química Analítica I"] },
-  "Termodinámica": { semestre: 2, desbloquea: ["Equilibrio y Cinética"] },
-
-  "Ecuaciones Diferenciales": { semestre: 3 },
-  "Equilibrio y Cinética": { semestre: 3 },
-  "Fundamentos de Espectroscopia": { semestre: 3, desbloquea: ["Química Cuántica I"] },
-  "Química Inorgánica I": { semestre: 3, desbloquea: ["Química Inorgánica II"] },
-  "Química Orgánica I": { semestre: 3, desbloquea: ["Química Orgánica II"] },
-
-  "Estadística": { semestre: 4, desbloquea: ["Metrología"] },
-  "Fisicoquímica de Iónica y Electródica": { semestre: 4 },
-  "Química Analítica I": { semestre: 4, desbloquea: ["Química Analítica II"] },
-  "Química Cuántica I": { semestre: 4 },
-  "Química Inorgánica II": { semestre: 4, desbloquea: ["Química Inorgánica III"] },
-  "Química Orgánica II": { semestre: 4, desbloquea: ["Química Orgánica III"] },
-
-  "Fisicoquímica de Interfases": { semestre: 5 },
-  "Metrología": { semestre: 5 },
-  "Química Analítica II": { semestre: 5, desbloquea: ["Química Analítica III", "Química Analítica Instrumental I", "Química Analítica Instrumental II"] },
-  "Química Inorgánica III": { semestre: 5, desbloquea: ["Química Inorgánica IV"] },
-  "Química Orgánica III": { semestre: 5, desbloquea: ["Química Orgánica IV"] },
-  "Optativa Sociohumanística": { semestre: 5 },
-
-  "Analítica Experimental I": { semestre: 6, desbloquea: ["Analítica Experimental II", "Analítica Experimental III"] },
-  "Cinética Química": { semestre: 6, desbloquea: ["Lab. Unificado de Fisicoquímica"] },
-  "Química Analítica III": { semestre: 6 },
-  "Química Inorgánica IV": { semestre: 6 },
-  "Química Orgánica IV": { semestre: 6, desbloquea: ["Bioquímica General"] },
-  "Optativa Sociohumanística": { semestre: 6 },
-
-  "Comunicación Científica": { semestre: 7 },
-  "Química Analítica Instrumental I": { semestre: 7 },
-  "Analítica Experimental II": { semestre: 7 },
-  "Bioquímica General": { semestre: 7 },
-  "Lab. Unificado de Fisicoquímica": { semestre: 7 },
-  "Optativa Disciplinaria Tipo A": { semestre: 7 },
-
-  "Analítica Experimental III": { semestre: 8 },
-  "Química Analítica Instrumental II": { semestre: 8 },
-  "Seminario I": { semestre: 8, desbloquea: ["Seminario II"] },
-  "Trabajo de Investigación I": { semestre: 8, desbloquea: ["Trabajo de Investigación II"] },
-  "Optativas Disciplinarias Tipo B": { semestre: 8 },
-
-  "Trabajo de Investigación II": { semestre: 9 },
-  "Seminario II": { semestre: 9 },
-  "Optativas Disciplinarias Tipo B": { semestre: 9 },
-  "Química de Coordinación": { semestre: 9 },
-  "Química Organometálica": { semestre: 9 },
-  "Química Covalente": { semestre: 9 },
-  "Química del Estado Sólido": { semestre: 9 },
-  "Métodos Electrométricos de Análisis": { semestre: 9 },
-  "Métodos Espectroscópicos Cuantitativos": { semestre: 9 },
-  "Métodos Espectroscópicos Estructurales": { semestre: 9 },
-  "Métodos Analíticos de Separación": { semestre: 9 }
+  1: [
+    { nombre: "Álgebra Superior", desbloquea: ["Termodinámica"] },
+    { nombre: "Cálculo I", desbloquea: ["Cálculo II", "Ecuaciones Diferenciales"] },
+    { nombre: "Ciencia y Sociedad" },
+    { nombre: "Física I", desbloquea: ["Física II", "Lab. de Física"] },
+    { nombre: "Química General I", desbloquea: ["Química General II"] },
+    { nombre: "La Universidad Libre de Violencia de Género" }
+  ],
+  2: [
+    { nombre: "Cálculo II" },
+    { nombre: "Estructura de la Materia", desbloquea: ["Química Inorgánica I", "Química Orgánica I"] },
+    { nombre: "Física II", desbloquea: ["Fundamentos de Espectroscopia"] },
+    { nombre: "Lab. de Física", desbloquea: ["Metrología"] },
+    { nombre: "Química General II", desbloquea: ["Química Analítica I"] },
+    { nombre: "Termodinámica", desbloquea: ["Equilibrio y Cinética"] }
+  ],
+  3: [
+    { nombre: "Ecuaciones Diferenciales" },
+    { nombre: "Equilibrio y Cinética" },
+    { nombre: "Fundamentos de Espectroscopia", desbloquea: ["Química Cuántica I"] },
+    { nombre: "Química Inorgánica I", desbloquea: ["Química Inorgánica II"] },
+    { nombre: "Química Orgánica I", desbloquea: ["Química Orgánica II"] }
+  ],
+  4: [
+    { nombre: "Estadística", desbloquea: ["Metrología"] },
+    { nombre: "Fisicoquímica de Iónica y Electródica" },
+    { nombre: "Química Analítica I", desbloquea: ["Química Analítica II"] },
+    { nombre: "Química Cuántica I" },
+    { nombre: "Química Inorgánica II", desbloquea: ["Química Inorgánica III"] },
+    { nombre: "Química Orgánica II", desbloquea: ["Química Orgánica III"] }
+  ],
+  5: [
+    { nombre: "Fisicoquímica de Interfases" },
+    { nombre: "Metrología" },
+    { nombre: "Química Analítica II", desbloquea: ["Química Analítica III", "Química Analítica Instrumental I", "Química Analítica Instrumental II"] },
+    { nombre: "Química Inorgánica III", desbloquea: ["Química Inorgánica IV"] },
+    { nombre: "Química Orgánica III", desbloquea: ["Química Orgánica IV"] },
+    { nombre: "Optativa Sociohumanística" }
+  ],
+  6: [
+    { nombre: "Analítica Experimental I", desbloquea: ["Analítica Experimental II", "Analítica Experimental III"] },
+    { nombre: "Cinética Química", desbloquea: ["Lab. Unificado de Fisicoquímica"] },
+    { nombre: "Química Analítica III" },
+    { nombre: "Química Inorgánica IV" },
+    { nombre: "Química Orgánica IV", desbloquea: ["Bioquímica General"] },
+    { nombre: "Optativa Sociohumanística" }
+  ],
+  7: [
+    { nombre: "Comunicación Científica" },
+    { nombre: "Química Analítica Instrumental I" },
+    { nombre: "Analítica Experimental II" },
+    { nombre: "Bioquímica General" },
+    { nombre: "Lab. Unificado de Fisicoquímica" },
+    { nombre: "Optativa Disciplinaria Tipo A" }
+  ],
+  8: [
+    { nombre: "Analítica Experimental III" },
+    { nombre: "Química Analítica Instrumental II" },
+    { nombre: "Seminario I", desbloquea: ["Seminario II"] },
+    { nombre: "Trabajo de Investigación I", desbloquea: ["Trabajo de Investigación II"] },
+    { nombre: "Optativas Disciplinarias Tipo B" }
+  ],
+  9: [
+    { nombre: "Trabajo de Investigación II" },
+    { nombre: "Seminario II" },
+    { nombre: "Optativas Disciplinarias Tipo B" },
+    { nombre: "Química de Coordinación" },
+    { nombre: "Química Organometálica" },
+    { nombre: "Química Covalente" },
+    { nombre: "Química del Estado Sólido" },
+    { nombre: "Métodos Electrométricos de Análisis" },
+    { nombre: "Métodos Espectroscópicos Cuantitativos" },
+    { nombre: "Métodos Espectroscópicos Estructurales" },
+    { nombre: "Métodos Analíticos de Separación" }
+  ]
 };
 
 const aprobadas = new Set();
@@ -71,21 +81,31 @@ const botones = {};
 
 function crearMalla() {
   const contenedor = document.getElementById("malla");
-  const ordenadas = Object.keys(materias).sort((a, b) => materias[a].semestre - materias[b].semestre);
 
-  ordenadas.forEach(nombre => {
-    const div = document.createElement("div");
-    div.textContent = nombre;
-    div.classList.add("materia");
-    div.dataset.nombre = nombre;
+  Object.keys(materias).forEach(semestre => {
+    const divSemestre = document.createElement("div");
+    divSemestre.classList.add("semestre");
 
-    if (materias[nombre].desbloquea && materias[nombre].desbloquea.length) {
-      div.title = "Desbloquea: " + materias[nombre].desbloquea.join(", ");
-    }
+    const titulo = document.createElement("h2");
+    titulo.textContent = `${semestre}° Semestre`;
 
-    div.addEventListener("click", () => toggleAprobada(nombre));
-    botones[nombre] = div;
-    contenedor.appendChild(div);
+    const divMaterias = document.createElement("div");
+    divMaterias.classList.add("materias");
+
+    materias[semestre].forEach(m => {
+      const div = document.createElement("div");
+      div.classList.add("materia");
+      div.textContent = m.nombre;
+      div.dataset.nombre = m.nombre;
+      div.addEventListener("click", () => toggleAprobada(m.nombre));
+
+      botones[m.nombre] = div;
+      divMaterias.appendChild(div);
+    });
+
+    divSemestre.appendChild(titulo);
+    divSemestre.appendChild(divMaterias);
+    contenedor.appendChild(divSemestre);
   });
 
   actualizarBloqueos();
@@ -106,10 +126,15 @@ function toggleAprobada(nombre) {
 }
 
 function actualizarBloqueos() {
-  Object.keys(materias).forEach(nombre => {
-    const prerequisitos = Object.keys(materias).filter(m =>
-      materias[m].desbloquea && materias[m].desbloquea.includes(nombre)
-    );
+  Object.keys(botones).forEach(nombre => {
+    const prerequisitos = [];
+    Object.values(materias).forEach(lista => {
+      lista.forEach(m => {
+        if (m.desbloquea && m.desbloquea.includes(nombre)) {
+          prerequisitos.push(m.nombre);
+        }
+      });
+    });
 
     const desbloqueada = prerequisitos.every(p => aprobadas.has(p)) || prerequisitos.length === 0;
 
